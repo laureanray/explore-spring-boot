f4acc9d2c621bafe7ed2366940f8de1c393f1413
laureanray <laureanraybahala@gmail.com>
2023-11-06 14:06:27 +0800

    init project

---
+++ src/main/java/com/laureanray/exploresb/ExploresbApplication.java
@@ -0,0 +1,13 @@
+package com.laureanray.exploresb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploresbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploresbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laureanray/exploresb/ExploresbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laureanray.exploresb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploresbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
